0b11e45a91eafff2e97a01586b702ed6c68c2bf7
Vadim <vadimshdrn@gmail.com>
2023-03-04 20:45:33 +0600

    Declare function that input on console array with books

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
-let myLibrary = [];
+let myLibrary = ['hobbit', 'star wars', 'harry potter', 'warhammer40000'];
 
 function Book(title, author, pages) {
- this.title = title;
- this.author = author;
- this.pages = pages;
- this.read = function() {
-     let reading = false;
-     if (!reading) {
-         console.log('Book is read');
-     }
- };
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = function () {
+        let reading = false;
+        if (!reading) {
+            console.log('Book is read');
+        }
+    };
 }
 
-function addBookToLibrary() {
+function addBookToLibrary(library) {
 
+    for (let book in library) {
+        console.log(library[book]);
+    }
 }
+addBookToLibrary(myLibrary);
