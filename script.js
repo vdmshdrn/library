5bbad1d4cc3ee5cfb03e822b6f168072148ea817
Vadim <vadimshdrn@gmail.com>
2023-03-08 12:03:33 +0600

    Implemented the visible form after press button

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
-let myLibrary = ['hobbit', 'star wars', 'harry potter', 'warhammer40000'];
+let myLibrary = [];
+let wrapper = document.querySelector('.wrapper');
+let addBookButton = document.querySelector(".add-book");
+
+addBookButton.addEventListener('click', function () {
+    wrapper.style.display = "block";
+    console.log('pressed');
+});
 
 function Book(title, author, pages) {
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.read = function () {
-        let reading = false;
-        if (!reading) {
-            console.log('Book is read');
-        }
-    };
+    this.read = read;
 }
 
 function addBookToLibrary(library) {
